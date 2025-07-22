ec4830b9a22b2adb0d386c9538837c92b7e479d1
Pattnaik <shupattnaik@deloitte.com>
2025-07-22 14:22:57 +0530

    Updated the Logo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import workflowLogo from '../../assets/images/workflow.png'
+import workflowLogo from '../../assets/images/workflow-logo.png'
 import './Home.css'
 const Home = () => {
     return(
@@ -8,7 +8,7 @@ const Home = () => {
                 <div className='row justify-content-center'>
                     <div className="col-12 col-md-6 col-lg-5 mb-4">
                         <div className="card p-2 mt-2" >
-                            <img className="card-img-top" src={workflowLogo} alt="Card image cap" style={{width: 200}} />
+                            <img className="card-img-top" src={workflowLogo} alt="Card image cap" style={{width: '100%'}} />
                             <div className="card-body">
                                 <h5 className="card-title">Interface Naming Tool</h5>
                                 <p className="card-text">This is an application using which you can generate the interface namings and its respective low level workflows</p>
